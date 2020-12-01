ec3e5ddf388d4191df69c29cb30a65f86e70add6
Morgan <khajiit11@gmail.com>
2020-12-01 15:23:22 +0000

    fixed caching issue

--- django_project/library/views.py
+++ django_project/library/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from library.models import *
+from django.views.decorators.cache import never_cache
 
 # Create your views here.
+@never_cache
 def index(request):
 	#return HttpResponse("Hello, world. You're at the library index!")
 	lib1 = Book.objects.filter(author="Tamora Pierce")
