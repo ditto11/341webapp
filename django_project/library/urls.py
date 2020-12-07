8df22889cd453127b91b65f06b3fabdb5fb70952
lizwat <emw119@case.edu>
2020-12-06 20:34:09 -0500

    search results page

--- django_project/library/urls.py
+++ django_project/library/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('?search/', views.search, name='search'),
+    path('', HomePageView.as_view(), name='home'),
+    path('?search/', SearchView.as_view(), name='search'),
 ]
--- django_project/library/views.py
+++ django_project/library/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from library.models import *
 from django.db.models import Q
 from django.views.decorators.cache import never_cache
+from django.view.generic import TemplateView, ListView
 
 # Create your views here.
 @never_cache
@@ -15,12 +16,18 @@ from django.views.decorators.cache import never_cache
 #	htmlString += "</body>"
 #	return HttpResponse(htmlString)
 
-def home(request):
-    books = Book.objects.all()
-    search_t = ''
-    if 'search' in request.GET:
-        search_t = request.GET['search']
-        books = Book.objects.filter(title__icontains=search_t)
+class SearchView(ListView):
+        model = Book
+        template_name = 'search.html'
+        
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+#def home(request):
+    #books = Book.objects.all()
+    #search_t = ''
+    #    search_t = request.GET['search']
+    #    books = Book.objects.filter(title__icontains=search_t)
         
         
    # context = {
@@ -28,13 +35,21 @@ def home(request):
        # 'search_t': search_t
        # }
     
-    return render(request, 'home.html')
+   # return render(request, 'home.html')
 
-def search(request):
-    query = request.GET.get('q', '')
-    if query:
-        queryset = (Q(title__icontains=query))|(Q(author__icontains=query))
-        results = Book.objects.filter(queryset).distinct()
-    else:
-        results = []
-    return render(request, 'search.html', {'results': results })
+def get_queryset(self):
+    query = self.request.GET.get('q')
+    object_list = Book.objects.filter(
+        Q(title_icontains=query | Q(author_icontains=query)
+)
+        return object_list
+
+#def search(request):
+   # books = Book.objects.all()
+   # query = request.GET.get("q")
+   # if query:
+       # books = books.filter(Q(title__icontains=query))|(Q(author__icontains=query)).distinct()
+        #return render(request, 'search.html', {'books':books})
+   # else:
+        #return render(request, 'search.html', {'books': books })
+#
