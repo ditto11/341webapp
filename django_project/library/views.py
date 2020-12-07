ee7647ca9c23aeebc599165e7f31e02b99a43450
lizwat <emw119@case.edu>
2020-12-06 20:59:07 -0500

    fix errors

--- django_project/library/views.py
+++ django_project/library/views.py
@@ -16,10 +16,10 @@ from django.view.generic import TemplateView, ListView
 #	htmlString += "</body>"
 #	return HttpResponse(htmlString)
 
-class SearchView(ListView):
-        model = Book
-        template_name = 'search.html'
-        
+#class SearchView(ListView):
+      #  model = Book
+       # template_name = 'search.html'
+        #
 #class HomePageView(TemplateView):
     #template_name = 'home.html'
 
@@ -38,19 +38,18 @@ def home(request):
     
     return render(request, 'home.html')
 
-def get_queryset(self):
-    query = self.request.GET.get('q')
-    object_list = Book.objects.filter(
-        Q(title_icontains=query | Q(author_icontains=query)
-)
-        return object_list
+#def get_queryset(self):
+   # query = self.request.GET.get('q')
+   # object_list = Book.objects.filter(
+      #  Q(title_icontains=query | Q(author_icontains=query)
+#)#
+        #return object_list
 
-#def search(request):
-   # books = Book.objects.all()
-   # query = request.GET.get("q")
-   # if query:
-       # books = books.filter(Q(title__icontains=query))|(Q(author__icontains=query)).distinct()
-        #return render(request, 'search.html', {'books':books})
-   # else:
-        #return render(request, 'search.html', {'books': books })
-#
+def search(request):
+    books = Book.objects.all()
+    query = request.GET.get("q")
+    if query:
+        books = books.filter(Q(title__icontains=query))|(Q(author__icontains=query)).distinct()
+        return render(request, 'search.html', {'books':books})
+    else:
+        return render(request, 'search.html', {'books': books })
